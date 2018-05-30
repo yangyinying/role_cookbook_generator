69f7e543c3411812a5bc8426fe2b4ec39d8ca747
yinying.yang <yinying.yang@salesforce.com>
2018-05-30 13:41:41 -0400

    just a thought

---
+++ recipes/recipe.rb
@@ -0,0 +1,12 @@
+
+context = ChefDK::Generator.context
+cookbook_dir = File.join(context.cookbook_root, context.cookbook_name)
+recipe_path = File.join(cookbook_dir, 'recipes', "#{context.new_file_basename}.rb")
+
+silence_chef_formatter unless context.verbose
+
+# Recipe
+template recipe_path do
+  source 'recipe.rb.erb'
+  helpers(ChefDK::Generator::TemplateHelper)
+end
